6757ae8d2bd4726387a29c13a8eaa3649382c16f
wslmacieira <wslmacieira@outlook.com>
2021-04-25 01:21:30 -0300

    criando projeto, controller entities database

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+import './database';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+/**
+ * GET = Buscas
+ * POST =  Criação
+ * PUT = Alteração
+ * DELETE =  Deletar
+ * PATCH = Alterar uma informação específica
+ */
+
+app.get('/', (request, response) => {
+    return response.json({
+        message: 'Olá NLW 05!'
+    });
+});
+
+app.post('/', (request, response) => {
+    return response.json({ message: 'Usúario salvo com sucesso!' });
+});
+
+app.listen(3333, () => console.log('Server is running on port 3333'));
